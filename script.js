ddf5891efe51ef2c912a7002a3dbd9d58546fee1
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-28 16:58:38 +0200

    commenti e  active

--- script.js
+++ script.js
@@ -5,18 +5,29 @@ cambia immagine automaticamente.
 */
 
 
-
+//inizializziamo Vue;
 const root = new Vue ({
     el: "#root",
     data: {
-        image: [
+        //posizione di partenza;
+        currentIndex: 0,
+        // array di immagini 
+        images: [
             "./img/image1.jpg",
-            // "./img/image2.jpg",
-            // "./img/image3.jpg",
-            // "./img/image4.jpg",
+            "./img/image2.jpg",
+            "./img/image3.jpg",
+            "./img/image4.jpg",
         ],
     },
     methods: {
+        // metodo per ritornare active se la current index corrisponde alla posizione in cui ci troviamo
+        isActive(index){
+            if(this.currentIndex === index) {
+                return "active"
+            } return " ";
+            //scritta in ternario
+        // return this.currentIndex === index ? "active" : " ";
+        }
         
     } 
 })
