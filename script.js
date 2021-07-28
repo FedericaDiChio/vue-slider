7961b4e8c8646ab524008f510fcaa8bdac874099
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-28 15:30:19 +0200

    html e data vue

--- script.js
+++ script.js
@@ -8,5 +8,16 @@ cambia immagine automaticamente.
 
 const root = new Vue ({
     el: "#root",
+    data: {
+        image: [
+            "./img/image1.jpg",
+            "./img/image2.jpg",
+            "./img/image3.jpg",
+            "./img/image4.jpg",
+        ],
+    },
+    methods: {
+        
+    } 
+})
     
-});    
